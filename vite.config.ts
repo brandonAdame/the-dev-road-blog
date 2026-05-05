be0fd6ac7e96a0f97d6267ccf1ab8b7722276b02
Brandon Adame <badamegachuz@gmail.com>
2026-05-05 13:44:31 -0400

    Updating dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { nitro } from "nitro/vite";
-// import netlify from "@netlify/vite-plugin-tanstack-start";
 
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
