f770c8acf13e0446131ae3f1026b2ec17aad5a6c
Brandon Adame <badamegachuz@gmail.com>
2026-05-09 19:43:13 -0400

    removing some imports

--- src/lib/markdown.ts
+++ src/lib/markdown.ts
@@ -1,11 +1,5 @@
 import * as Marked from "marked";
 import Prism from "prismjs";
-import "prismjs/components/prism-javascript";
-import "prismjs/components/prism-typescript";
-import "prismjs/components/prism-jsx";
-import "prismjs/components/prism-tsx";
-import "prismjs/components/prism-css";
-import "prismjs/components/prism-markdown";
 
 export interface MarkdownHeading {
   id: string;
